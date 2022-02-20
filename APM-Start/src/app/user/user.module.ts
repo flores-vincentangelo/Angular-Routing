286d16244cb6f8f0de029b189d18dad2a84a7df2
flores-vincentangelo <vincent.flores@pointwest.com.ph>
2022-02-20 23:17:44 +0800

    updates

--- APM-Start/src/app/user/user.module.ts
+++ APM-Start/src/app/user/user.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './login.component';
 
 import { SharedModule } from '../shared/shared.module';
 
+const routes: Routes = [
+  { path: 'login', component: LoginComponent }
+]
+
 @NgModule({
   imports: [
-    SharedModule
+    SharedModule,
+    RouterModule.forChild(routes)
   ],
   declarations: [
     LoginComponent
