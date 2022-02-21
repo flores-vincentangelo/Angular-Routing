5ff2a13c1f7cbd978dcb68a2a5deb683aa5c9b64
flores-vincentangelo <vincent.flores@pointwest.com.ph>
2022-02-21 17:40:42 +0800

    Product-resolver

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -6,11 +6,22 @@ import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 
 import { SharedModule } from '../shared/shared.module';
+import { ProductResolverService } from './product-resolver.service';
+
+
 
 const routes: Routes = [
   { path: 'products', component: ProductListComponent },
-  { path: 'products/:id', component: ProductDetailComponent },
-  { path: 'products/:id/edit', component: ProductEditComponent }
+  {
+    path: 'products/:id',
+    component: ProductDetailComponent,
+    resolve: { resolvedData: ProductResolverService }
+  },
+  {
+    path: 'products/:id/edit',
+    component: ProductEditComponent,
+    resolve: { resolvedData: ProductResolverService }
+  }
 ];
 
 @NgModule({
