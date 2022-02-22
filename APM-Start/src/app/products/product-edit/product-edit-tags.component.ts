8c8aacf528ecfbcfb5f6960972d68affec34bdd9
flores-vincentangelo <vincent.flores@pointwest.com.ph>
2022-02-22 10:44:48 +0800

    updates

--- APM-Start/src/app/products/product-edit/product-edit-tags.component.ts
+++ APM-Start/src/app/products/product-edit/product-edit-tags.component.ts
@@ -9,11 +9,18 @@ import { Product } from '../product';
 export class ProductEditTagsComponent implements OnInit {
   errorMessage: string;
   newTags = '';
-  product = { id: 1, category: 'test', tags: ['test'] };
+  product: Product;
 
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.parent.data.subscribe({
+      next: data => {
+        this.product = data['resolvedData'].product;
+        this.errorMessage = data['resolvedData'].error;
+      },
+      error: err => this.errorMessage = err
+    });
   }
 
   // Add the defined tags
