c57726bf2230961c00fa2b358e02da6f15fbd389
flores-vincentangelo <vincent.flores@pointwest.com.ph>
2022-02-22 17:14:25 +0800

    authentication guard

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -10,12 +10,14 @@ import { ProductResolverService } from './product-resolver.service';
 import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
 import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
 import { ProductListResolverService } from './product-list-resolver.service';
+import { AuthGuard } from '../user/auth.guard';
 
 
 
 const routes: Routes = [
   {
     path: 'products',
+    canActivate: [AuthGuard],
     children: [
       {
         path: '',
